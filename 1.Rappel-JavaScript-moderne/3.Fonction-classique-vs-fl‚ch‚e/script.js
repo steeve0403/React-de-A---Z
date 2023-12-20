f750caa2887222a3ab9fb53d4c1554b1eef8a089
steeve0304 <zychsteeve4@gmail.com>
2023-12-20 23:13:15 +0100

    commit for commit

--- 1.Rappel-JavaScript-moderne/3.Fonction-classique-vs-fl‚ch‚e/script.js
+++ 1.Rappel-JavaScript-moderne/3.Fonction-classique-vs-fl‚ch‚e/script.js
@@ -49,7 +49,6 @@ function faz() {
 const foz = () => {
     console.log(arguments)
 }
-suppersjkdv:Bvkemb
 /*
     Petit recap pour vous aider avec la valeur de this dans des fonctions classiques vs fléchées (hors fonction constructeur / usestrict)
 
