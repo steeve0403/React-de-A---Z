a1ecfb02ff334b148f6ac0de77d8b5a5f515fa6a
steeve0304 <zychsteeve4@gmail.com>
2023-12-26 15:27:44 +0100

    Modules Es6

---
+++ 1.Rappel-JavaScript-moderne/12.Modules-es6/module.js
@@ -0,0 +1,10 @@
+const age = 45
+let userName = "Giovanni"
+
+export default function  foo() {
+    console.log("Hello World")
+}
+
+export {age as exactAge, userName}
+
+export
---
+++ 1.Rappel-JavaScript-moderne/12.Modules-es6/script.js
@@ -0,0 +1,8 @@
+// import {exactAge as newAge, userName} from "./module";
+// import foo from "./module";
+import * as Utils from "./module"
+
+// foo()
+// console.log(newAge, userName)
+
+import "./module"
