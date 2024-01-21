878b445956f8878df3c9e26ee200af88ca34ce8c
steeve0304 <zychsteeve4@gmail.com>
2024-01-21 10:35:27 +0100

    UnControlled inputs

--- 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
@@ -7,10 +7,16 @@ export default function Container() {
     function handleInput(e) {
         setState(e.target.value)
     }
+
+    const emailRef = useRef()
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(e.target.value)
+    }
   return (
     <div>
       <h1>Composants "controlled" et "uncontrolled"</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="psdeudo">Votre nom</label>
             <input
                 onChange={handleInput}
@@ -18,7 +24,16 @@ export default function Container() {
                 type="text"
                 id="pseudo"
             />
-            {!state && <p>Veuillez remplir cet input</p>}
+
+            {/* {!state && <p>Veuillez remplir cet input</p>} */}
+
+            <label htmlFor="email">Votre email</label>
+            <input
+                ref={emailRef}
+                type="email"
+                id="email"
+            />
+
             <button>Envoyer</button>
         </form>
         <p>Votre pseudo : {state}</p>
