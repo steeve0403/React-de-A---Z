b60af2416ab31a32567a8d9f5e052fe4f6464d45
steeve0304 <zychsteeve4@gmail.com>
2024-01-21 09:08:56 +0100

    Ternary operation

--- 3.Les-bases-de-React/src/lessons/10.Opérateur-ternaire/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/10.Opérateur-ternaire/Container.jsx
@@ -2,10 +2,24 @@ import { useState } from "react"
 import "./Container.css"
 
 export default function Container() {
+
+    const [validation, setValidation] = useState(false)
+
+
+
   return (
     <div>
       <h1>Opération ternaire</h1>
-      <form></form>
+      <form>
+          <label htmlFor="consent">Acceptez-vous ?</label>
+          <input onClick={() => setValidation(!validation)} type="checkbox" />
+          <p className={`${validation ? "valid" : "invalid"}`}
+              // style={{color: `${validation ? "lightgreen" : "red"}`}}
+          >
+              {validation ? "Bravo, vous pouvez envoyer le formulaire" : "Vous devez la case"}
+          </p>
+          {validation ? <p>True</p> : <p>False</p>}
+      </form>
     </div>
   )
 }
