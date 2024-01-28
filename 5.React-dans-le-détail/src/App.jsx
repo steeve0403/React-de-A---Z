4b970786886e98c3aabac773e351d6f7a51916b9
steeve0304 <zychsteeve4@gmail.com>
2024-01-28 11:51:15 +0100

    react fragment

--- 5.React-dans-le-détail/src/App.jsx
+++ 5.React-dans-le-détail/src/App.jsx
@@ -1,8 +1,9 @@
+import Container from "./lessons/1.Les-fragments/Container.jsx";
 function App() {
 
   return (
     <div className="main-content">
- 
+        <Container />
     </div>
   )
 }
--- 5.React-dans-le-détail/src/lessons/1.Les-fragments/Container.jsx
+++ 5.React-dans-le-détail/src/lessons/1.Les-fragments/Container.jsx
@@ -2,6 +2,9 @@ export default function Container() {
   return (
     <div>
       <h1>Les fragments</h1>
+        <p>
+            Les fragments permettent de retourner plusieurs éléments sans avoir à les encapsuler dans un élément parent.
+        </p>
     </div>
   )
 }
