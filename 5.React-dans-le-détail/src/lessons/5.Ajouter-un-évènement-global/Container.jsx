f0d40c5021456fe23d642bed413421bec99fc70f
steeve0304 <zychsteeve4@gmail.com>
2024-01-28 14:04:27 +0100

    Global event

--- 5.React-dans-le-détail/src/lessons/5.Ajouter-un-évènement-global/Container.jsx
+++ 5.React-dans-le-détail/src/lessons/5.Ajouter-un-évènement-global/Container.jsx
@@ -1,10 +1,16 @@
+import React,{ useState }  from "react";
+import HugeContent from "./HugeContent.jsx";
 
 export default function Container() {
+    const [showContent, setShowContent] = useState(false);
 
   return (
     <div>
       <h1>Ajouter un évènement global</h1>
-
+        <button onClick={() => setShowContent(!showContent)}>
+            {showContent ? "Hide Content" : "Show Content"}
+        </button>
+        {showContent && <HugeContent />}
     </div>
   )
 }
