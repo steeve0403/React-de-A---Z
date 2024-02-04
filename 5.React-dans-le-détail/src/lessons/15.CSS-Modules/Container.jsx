0ce52101030b59b803fd0b293a4a620724820204
steeve0304 <zychsteeve4@gmail.com>
2024-02-04 16:46:26 +0100

    css modules

--- 5.React-dans-le-détail/src/lessons/15.CSS-Modules/Container.jsx
+++ 5.React-dans-le-détail/src/lessons/15.CSS-Modules/Container.jsx
@@ -1,8 +1,10 @@
+import "./Container.css"
+import styles from "./Container.module.css"
 export default function Container() {
 
   return (
     <div>
-      <h1>CSS Modules</h1>
+      <h1 className={styles.title}>CSS Modules</h1>
     </div>
   )
 }
