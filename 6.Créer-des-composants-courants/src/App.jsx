bf51ece382386df339595c734f0b817d00c19aff
steeve0403 <zychsteeve4@gmail.com>
2024-02-11 18:59:10 +0100

    create modal component

--- 6.Créer-des-composants-courants/src/App.jsx
+++ 6.Créer-des-composants-courants/src/App.jsx
@@ -1,8 +1,9 @@
+import ModalButton from "./lessons/1.Créer-une-fenêtre-modale-avec-un-portal/ModalButon.jsx";
 function App() {
 
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
-
+      <ModalButton />
     </div>
   )
 }
