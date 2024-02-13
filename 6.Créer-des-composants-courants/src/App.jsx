6326aa78f3df7abaf921b849182db29dde12fa48
steeve0403 <zychsteeve4@gmail.com>
2024-02-13 19:11:11 +0100

    create tab system component

--- 6.Créer-des-composants-courants/src/App.jsx
+++ 6.Créer-des-composants-courants/src/App.jsx
@@ -1,9 +1,9 @@
-import ModalButton from "./lessons/1.Créer-une-fenêtre-modale-avec-un-portal/ModalButon.jsx";
+import Tabs from "./lessons/2.Coder-des-onglets/Tabs.jsx";
 function App() {
 
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
-      <ModalButton />
+      <Tabs />
     </div>
   )
 }
