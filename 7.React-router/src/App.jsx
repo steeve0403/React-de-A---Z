3f236748d1d0999211b64d2425602d65a93f0d08
steeve0403 <zychsteeve4@gmail.com>
2024-02-17 13:07:01 +0100

    add routes on project

--- 7.React-router/src/App.jsx
+++ 7.React-router/src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
