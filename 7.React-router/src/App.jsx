d81c6a6abdc067d23cd218e9a299485bd95a7f07
steeve0403 <zychsteeve4@gmail.com>
2024-02-17 17:45:35 +0100

    404 not found

--- 7.React-router/src/App.jsx
+++ 7.React-router/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home.jsx"
 import Contact from "./components/Contact.jsx"
-import UserProfile from "./components/UserProfile.jsx";
+import UserProfile from "./components/UserProfile.jsx"
+import NotFound from "./components/NotFound.jsx"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/profile/:id" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
           </Routes>
       </BrowserRouter>
     </>
