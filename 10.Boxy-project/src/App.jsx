f22ca7310501a02e2bbf6d7276c52cb38ad3ba2f
steeve0403 <zychsteeve4@gmail.com>
2024-02-20 18:54:55 +0100

    start basic structure basic

--- 10.Boxy-project/src/App.jsx
+++ 10.Boxy-project/src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx";
+
 function App() {
   return (
-    <div>
-
+    <div className="min-h-screen flex-col">
+      <Header />
+      <main className="flex flex-wrap justify-center items-center flex-row bg-gray-100 px-10 md:flex-nowrap">
+        {/*<LeftSide />*/}
+        {/*<Visualisation />*/}
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ 10.Boxy-project/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (
+        <footer className="p-2 border-t-2 border-gray-200">
+            <p className="text-xsm text-center uppercase">
+                Boxy - All right reserved ©
+            </p>
+        </footer>
+    );
+}
