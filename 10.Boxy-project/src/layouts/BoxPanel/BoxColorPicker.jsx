5caf5122817c83e871443db159df0f0f8f55e634
steeve0403 <zychsteeve4@gmail.com>
2024-03-16 18:28:13 +0100

    Set up the input color related to the box

--- 10.Boxy-project/src/layouts/BoxPanel/BoxColorPicker.jsx
+++ 10.Boxy-project/src/layouts/BoxPanel/BoxColorPicker.jsx
@@ -1,9 +1,32 @@
+import { useDispatch } from "react-redux"
+import { updateBoxValue } from "../../features/boxProperties"
 
+export default function BoxColorPicker({inputData}) {
+    const dispatch = useDispatch()
 
-export default function BoxColorPicker() {
+    function handleInputs(e) {
+        dispatch(updateBoxValue({
+            inputNumber: inputData.inputNumber,
+            value: e.target.value
+        }))
+    }
     return (
-        <div className="">
-            BoxColorPicker
+        <div className="mt-3">
+            <p>{inputData.name}</p>
+            <div className="flex mt-2">
+                <input
+                    value={inputData.value}
+                    onChange={handleInputs}
+                    type="text"
+                    className="flex-grow border py-1 px-2 focus:outline-1 outline-gray-400"
+                />
+                <input
+                    value={inputData.value}
+                    onChange={handleInputs}
+                    type="color"
+                    className="cursor-pointer h-[40px]"
+                />
+            </div>
         </div>
-    )
+            )
 }
