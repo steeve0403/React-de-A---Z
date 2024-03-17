49bfb6ca95f231f469abf7476eb2ab1e092f7a2d
steeve0403 <zychsteeve4@gmail.com>
2024-03-17 14:55:26 +0100

    Manage the input range of shadows

--- 10.Boxy-project/src/layouts/ShadowList/ShadowRange.jsx
+++ 10.Boxy-project/src/layouts/ShadowList/ShadowRange.jsx
@@ -1,11 +1,45 @@
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
+import { updateShadowValue } from "../../features/shadows"
+import {updateBoxValue} from "../../features/boxProperties.js";
 
-export default function ShadowRange() {
+export default function ShadowRange({inputData, shadowId}) {
     const dispatch = useDispatch()
 
+    function handleInputs(e) {
+        dispatch(updateShadowValue({
+            inputNumber: inputData.inputNumber,
+            value: e.target.value,
+            shadowId
+        }))
+    }
+
     return (
-        <div className="">
-            ShadowRange
+        <div className="my-8">
+            <div className="flex justify-between">
+                <p>{inputData.name}</p>
+                <div className="flex items-baseline mb-2">
+                    <input
+                        value={inputData.value}
+                        onChange={handleInputs}
+                        type="number"
+                        className="w-14 h-8 mr-2 border border-gray-200 text-center"
+                    />
+                    <p>px</p>
+                </div>
+            </div>
+            <div className="relative z-0 w-full flex items-center">
+                <input
+                    value={inputData.value}
+                    onChange={handleInputs}
+                    type="range"
+                    min={inputData.minMax[0]}
+                    max={inputData.minMax[1]}
+                    className="w-full h-[2px] bg-gray-300 rounded-lg appearance-none cursor-pointer"
+                />
+                <div
+                    className="absolute -z-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-0.5 h-10 bg-gray-300 rounded">
+                </div>
+            </div>
         </div>
     )
 }
